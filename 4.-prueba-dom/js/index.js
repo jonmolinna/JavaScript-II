73dedb11e5b476c24f76819d36d5d67f0a5f0ffa
jonmolinna <jmolina2624@gmail.com>
2020-09-25 11:42:10 -0500

    Scroll

--- 4.-prueba-dom/js/index.js
+++ 4.-prueba-dom/js/index.js
@@ -1,6 +1,8 @@
 import hamburgerMenu from "./prueba1.js";
-
 import { digitalClock, alarm } from './prueba2.js';
+import { shortcuts, moveBall } from './prueba3.js';
+import countdown from './prueba4.js';
+import scrollTopButton from './prueba5.js'
 
 // 1.-Creando un menu de Hamburguesa
 document.addEventListener("DOMContentLoaded", e => {
@@ -8,4 +10,13 @@ document.addEventListener("DOMContentLoaded", e => {
 
     digitalClock('#reloj', '#activar-reloj', '#desactivar-reloj');
     alarm("assets/alarma.mp3", "#activar-alarma", "#desactivar-alarma");
+
+    countdown("countdown", "May 23, 2021 03:23:19", "Feliz Cumple :)")
+
+    scrollTopButton(".scroll-top-btn");
+});
+
+document.addEventListener("keydown", e => {
+    shortcuts(e);
+    moveBall(e, ".ball", ".stage");
 });
