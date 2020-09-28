ed87eae48a8dbb22de622416ec1c5a5e8ad871b4
jonmolinna <jmolina2624@gmail.com>
2020-09-28 11:28:44 -0500

    Dark y Ligth

--- 4.-prueba-dom/js/index.js
+++ 4.-prueba-dom/js/index.js
@@ -3,6 +3,7 @@ import { digitalClock, alarm } from './prueba2.js';
 import { shortcuts, moveBall } from './prueba3.js';
 import countdown from './prueba4.js';
 import scrollTopButton from './prueba5.js'
+import darkTheme from './prueba6.js';
 
 // 1.-Creando un menu de Hamburguesa
 document.addEventListener("DOMContentLoaded", e => {
@@ -14,9 +15,12 @@ document.addEventListener("DOMContentLoaded", e => {
     countdown("countdown", "May 23, 2021 03:23:19", "Feliz Cumple :)")
 
     scrollTopButton(".scroll-top-btn");
+
 });
 
 document.addEventListener("keydown", e => {
     shortcuts(e);
     moveBall(e, ".ball", ".stage");
 });
+
+darkTheme(".dark-theme-btn", "dark-mode");
