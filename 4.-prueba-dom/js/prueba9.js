841dbb45e73819d344675fcf0f05fc866066a620
jonmolinna <jmolina2624@gmail.com>
2020-10-01 11:57:40 -0500

    Detección de Dispositivos (User Agent)

---
+++ 4.-prueba-dom/js/prueba9.js
@@ -0,0 +1,73 @@
+/***** Detección de Dispositivos (User Agent) *****/
+export default function userDeviceInfo(id){
+    const $id = document.getElementById(id);
+    const isMobile = {
+        android: () => navigator.userAgent.match(/android/i),
+        ios: () => navigator.userAgent.match(/iphone|ipad|ipod/i),
+        windows: () => navigator.userAgent.match(/windows phone/i),
+        any: function(){
+            return this.android() || this.ios() || this.windows();
+        }
+    };
+    const isDesktop = {
+        linux: () => navigator.userAgent.match(/linux/i),
+        mac: () => navigator.userAgent.match(/mac os/i),
+        windows: () => navigator.userAgent.match(/windows nt/i),
+        any: function(){
+            return this.linux() || this.mac() || this.windows();
+        }
+    };
+    const isBrowser = {
+        chrome: () => navigator.userAgent.match(/chrome/i),
+        safari: () => navigator.userAgent.match(/safari/i),
+        firefox: () => navigator.userAgent.match(/firefox/i),
+        opera: () => navigator.userAgent.match(/opera|opera mini/i),
+        ie: () => navigator.userAgent.match(/msie|iemobile/i),
+        edge: () => navigator.userAgent.match(/edge/i),
+        any: function(){
+            return (
+                this.ie() ||
+                this.edge() ||
+                this.chrome() ||
+                this.safari() ||
+                this.firefox() ||
+                this.opera()
+            )
+        }
+    };
+
+    //console.log(isMobile.android());
+    $id.innerHTML = `
+        <ul>
+            <li>User Agent: <b>${navigator.userAgent}</b></li>
+            <li>Plataforma: <b>${isMobile.any()? isMobile.any() : isDesktop.any()}</b></li>
+            <li>Navegador: <b>${isBrowser.any()}</b></li>
+        </ul>
+    `;
+
+    // Contenido exclusivo
+    if(isBrowser.chrome()){
+        $id.innerHTML += `<p><mark>Este contenido solo se ve en chrome</mark></p>`
+    }
+
+    if(isBrowser.firefox()){
+        $id.innerHTML += `<p><mark>Este contenido solo se ve en Firefox</mark></p>`
+    }
+
+    if(isDesktop.linux()){
+        $id.innerHTML += `<p><mark>Descarga nuestro software para linux</mark></p>`
+    }
+
+    if(isDesktop.mac()){
+        $id.innerHTML += `<p><mark>Descarga nuestro software para windows</mark></p>`
+    }
+
+    if(isDesktop.windows()){
+        $id.innerHTML += `<p><mark>Descarga nuestro software para windows</mark></p>`
+    }
+
+    // Redirecciones
+    if(isMobile.android()){
+        window.location.href = "https://www.youtube.com/"
+    }
+}
