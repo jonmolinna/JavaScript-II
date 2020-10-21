286067f9eeadfa4d2c32766e95a0f685055e9cd7
jonmolinna <jmolina2624@gmail.com>
2020-10-21 08:45:56 -0500

    Sitio Web con Ajax

---
+++ 7.-prueba-ajax/pagina-web/1.-prueba1.js
@@ -0,0 +1,41 @@
+const $main = document.querySelector("main");
+
+const getHTML = (options) => {
+    let {url, success, error} = options;
+    const xhr = new XMLHttpRequest();
+
+    xhr.addEventListener("readystatechange", e => {
+        if(xhr.readyState !== 4) return;
+
+        if(xhr.status >= 200 && xhr.status < 300){
+            let html = xhr.responseText;
+            success(html);
+        } else {
+            let message = xhr.statusText || "Ocurrio un Error";
+            error(`Error ${xhr.status} : ${message}`);
+        }
+    });
+
+    xhr.open("GET", url);
+    xhr.setRequestHeader("Content-type", "text/html; charset=utf-8");
+    xhr.send();
+}
+
+document.addEventListener("DOMContentLoaded", e => {
+    getHTML({
+        url: "assets/home.html",
+        success: (html) => $main.innerHTML = html,
+        error: (err) => $main.innerHTML = `<h1>${err}</h1>`
+    });
+});
+
+document.addEventListener("click", e => {
+    if(e.target.matches(".menu a")){
+        e.preventDefault();
+        getHTML({
+            url: e.target.href,
+            success: (html) => $main.innerHTML = html,
+            error: (err) => $main.innerHTML = `<h1>${err}</h1>`
+        });
+    }
+});
