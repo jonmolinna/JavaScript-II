cbbc95a80ceab07b6bebb7169a136f3101b23158
jonmolinna <jmolina2624@gmail.com>
2020-11-03 11:16:49 -0500

    Objeto Math

---
+++ 0.-clases-herencia/5.-objeto-math.js
@@ -0,0 +1,28 @@
+//----------Metodo Math
+console.log(Math);
+console.log(Math.PI)
+
+// Devuelve el valor absoluto (cantidad)
+console.log(Math.abs(-7.8))
+
+// Metodos para el redondeo
+console.log(Math.ceil(7.3));
+console.log(Math.floor(7.8));
+console.log(Math.round(7.5));
+
+// Raiz cuadrada
+console.log(Math.sqrt(81));
+
+// Potencia
+console.log(Math.pow(2,5));
+
+// Verifica si el numero es positivo, negativo o cero
+console.log(Math.sign(-7.8));
+console.log(Math.sign(7.8));
+console.log(Math.sign(0));
+
+// Numero aleatorio (entre 0 y 1)
+console.log(Math.random());
+
+// Numero aleatorio entre 0 y 1000
+console.log(Math.round(Math.random() * 1000));
