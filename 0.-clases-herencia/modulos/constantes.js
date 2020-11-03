7a368735f87c56587b4affb5e92126e9a39e6136
jonmolinna <jmolina2624@gmail.com>
2020-11-03 11:15:38 -0500

    Constantes

---
+++ 0.-clases-herencia/modulos/constantes.js
@@ -0,0 +1,16 @@
+export const PI = Math.PI;
+
+export let usuario = "Eung";
+
+const password = "eung123";
+//export default password;
+
+export default function saludar(){
+    console.log("Hola Mudulos de ES6 de una funcion")
+}
+
+export class Saludar {
+    constructor(){
+        console.log("Hola Modulos ES6 de una clase")
+    }
+};
